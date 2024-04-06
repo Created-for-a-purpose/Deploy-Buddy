a3aeed561ffd11aa3b0d9b8140d7dba90a9bd364
ayushsingh82 <ayushsinghmi711@gmail.com>
2024-04-07 00:04:31 +0530

    select chains

--- frontend/src/components/Deploy/SelectChain.jsx
+++ frontend/src/components/Deploy/SelectChain.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import {Select, SelectItem} from "@nextui-org/react";
+import {animals} from "./data";
 
 function SelectChain() {
   return (
@@ -6,12 +8,31 @@ function SelectChain() {
       <div className='border border-red-100 w-2/3 '>
         <h1 className='mt-[25px] text-xl font-semibold'>Select Chain</h1>
 
-        <div className='flex justify-between mt-[25px] mb-[25px]'>
-          <button className='ml-[15px] border border-2 px-[10px] py-[10px]'><a href='/contract-details'>Back</a></button>
-          <button className='mr-[15px] border border-2 px-[10px] py-[10px]'><a href='/compile'>Next</a></button>
+        <div className='mt-[20px]'>
+        <h3 className='text-lg font-medium'>Deploy From </h3>
+         <div className='mt-[15px] text-lg'>
+         <Select
+         label="Listed Chains"
+         placeholder="Select chains"
+         selectionMode="multiple"
+         className="max-w-xs "
+       >
+         {animals.map((animal) => (
+           <SelectItem key={animal.value} value={animal.value}>
+             {animal.label}
+           </SelectItem>
+         ))}
+       </Select>
+         </div>
         </div>
+       
+        <div className='flex justify-between mt-[25px] mb-[25px]'>
+        <button className='ml-[15px] border border-2 px-[10px] py-[10px]'><a href='/contract-details'>Back</a></button>
+        <button className='mr-[15px] border border-2 px-[10px] py-[10px]'><a href='/compile'>Next</a></button>
+        </div>
+       </div>
       </div>
-    </div>
+   
   )
 }
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,7 @@ import './index.css'
 import './App.css'
 import { createBrowserRouter, RouterProvider, createRoutesFromElements } from 'react-router-dom'
 import { Route } from 'react-router-dom'
+import {NextUIProvider} from '@nextui-org/react'
 
 import Home from './components/Home.jsx'
 
@@ -58,14 +59,18 @@ const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
 
-    <WagmiProvider config={config}>
-      <QueryClientProvider client={queryClient}>
-        <RainbowKitProvider>
-          <Navbar />
-          <RouterProvider router={router} />
-        </RainbowKitProvider>
-      </QueryClientProvider>
-    </WagmiProvider>
+  <WagmiProvider config={config}>
+  <QueryClientProvider client={queryClient}>
+    <RainbowKitProvider>
+  
+    <NextUIProvider>
+    <Navbar/>
+    <RouterProvider router={router}/>
+  </NextUIProvider>
+  
+    </RainbowKitProvider>
+  </QueryClientProvider>
+  </WagmiProvider>
 
 
   </React.StrictMode>,
